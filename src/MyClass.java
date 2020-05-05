9d51c92f281be789e1764777ed73bc1211f7518d
Дмитрий <@192.168.1.5>
2020-05-05 12:27:44 +0300

    First Project

---
+++ src/MyClass.java
@@ -0,0 +1,27 @@
+
+public class MyClass {
+
+/*static void equal (String s1, String s2)
+{
+	if (equals(s1,s2))
+		System.out.println("Equals");
+	else
+		System.out.println("Not");
+}*/
+	public static void main(String[] args) {
+		for (int i = 3; i < 100; i++)
+		{
+			int chet = 0;
+			for(int j = 2; j < 100; j++ )
+			{
+				if ((i%j) == 0 && i != j)
+					break;
+				else chet = j;
+			}
+			if (chet==99)
+				System.out.println(i);
+		}
+	}
+}
+
+
